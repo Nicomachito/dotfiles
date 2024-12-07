485e84f6fa5da5204026da4fb764a348cc0210c3
Marcos Hernandez <marcos@mhndz.com>
2024-12-06 21:11:30 -0500

    added transparency

--- .config/nvim/lua/config/opt.lua
+++ .config/nvim/lua/config/opt.lua
@@ -32,3 +32,10 @@ vim.opt.colorcolumn = "80"
 
 vim.cmd.colorscheme 'melange'
 
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
